6b6cc30d5c4dba980e44ac9a8ba4ec6ed088b30c
Denis Konoplev <dekonoplyov@gmail.com>
2019-08-01 16:01:37 +0300

    Messy png write + stb libs

--- src/glow.cpp
+++ src/glow.cpp
@@ -1,9 +1,18 @@
 #include "makemeglow/glow.h"
 
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <stb_image_write.h>
+
+#include <cstdint>
+
 namespace glow {
 
 int foo()
 {
+    const size_t HEIGHT = 100;
+    const size_t WIDTH = 100;
+    uint8_t ramp[WIDTH * HEIGHT * 3];
+    stbi_write_png("./some.png", WIDTH, HEIGHT, 3, &ramp, 3 * WIDTH);
     return 42;
 }
 
