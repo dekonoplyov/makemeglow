05149eb44e6f5fbc338d4d2e0f4f3654e586ca9c
Denis Konoplev <dekonoplyov@gmail.com>
2019-08-06 01:41:48 +0300

    Gauss blur

--- src/glow.cpp
+++ src/glow.cpp
@@ -2,61 +2,71 @@
 #include "font_rasterizer.h"
 
 #include <algorithm>
+#include <array>
 #include <cstdint>
 #include <exception>
 #include <iostream>
 
 namespace glow {
 
-ColorBuffer glowText(IntensityBuffer buffer)
+namespace {
+
+ColorBuffer gaussianBlur(const IntensityBuffer& buffer, const std::vector<float>& weights)
 {
-    const size_t margin = 10;
-    ColorBuffer glowedBuffer{buffer.width() + 2 * margin, buffer.height() + 2 * margin};
+    const int yLimit = static_cast<int>(buffer.height());
+    const int xLimit = static_cast<int>(buffer.width());
+    const int radius = static_cast<int>(weights.size());
 
-    const float multiplier = 1.f / (margin * margin);
-    const int from = -static_cast<int>(3 * margin / 2);
-    const int to = static_cast<int>(margin) + from;
+    IntensityBuffer horizontalPass{buffer.width(), buffer.height()};
+    for (int y = 0; y < yLimit; ++y) {
+        for (int x = 0; x < xLimit; ++x) {
+            float res = static_cast<float>(buffer.at(x, y)) * weights[0];
+            for (int i = 1; i < radius; ++i) {
+                if (x + i < xLimit) {
+                    res += static_cast<float>(buffer.at(x + i, y)) * weights[i];
+                }
+                if (x - i >= 0) {
+                    res += static_cast<float>(buffer.at(x - i, y)) * weights[i];
+                }
+            }
+            horizontalPass.at(x, y) = static_cast<uint8_t>(std::clamp(res, 0.f, 255.f));
+        }
+    }
 
-    for (size_t x = 0; x < glowedBuffer.width(); ++x) {
-        for (size_t y = 0; y < glowedBuffer.height(); ++y) {
-            uint32_t intensityAcc = 0;
-
-            for (int i = from; i < to; ++i) {
-                for (int j = from; j < to; ++j) {
-                    int p = i + x;
-                    int q = j + y;
-                    if (p < 0 || p >= buffer.width() || q < 0 || q >= buffer.height()) {
-                        continue;
-                    }
-
-                    intensityAcc += buffer.at(p, q);
+    ColorBuffer verticalPass{buffer.width(), buffer.height()};
+    for (int y = 0; y < yLimit; ++y) {
+        for (int x = 0; x < xLimit; ++x) {
+            float res = static_cast<float>(horizontalPass.at(x, y)) * weights[0];
+            for (int i = 1; i < radius; ++i) {
+                if (y + i < yLimit) {
+                    res += static_cast<float>(horizontalPass.at(x, y + i)) * weights[i];
+                }
+                if (y - i >= 0) {
+                    res += static_cast<float>(horizontalPass.at(x, y - i)) * weights[i];
                 }
             }
 
-            uint8_t intensity = static_cast<uint8_t>(std::clamp(intensityAcc * multiplier, 0.f, 255.f));
-            if (x > margin && x < (buffer.width() + margin)
-                && y > margin && y < (buffer.height() + margin)) {
-                intensity = std::max(intensity, buffer.at(x - margin, y - margin));
-            }
-
-            Color c{
+            const auto intensity = std::max(static_cast<uint8_t>(std::clamp(res, 0.f, 255.f)), buffer.at(x, y));
+            const Color c{
                 static_cast<uint8_t>(231 * (intensity / 255.f)),
                 static_cast<uint8_t>(89 * (intensity / 255.f)),
                 static_cast<uint8_t>(163 * (intensity / 255.f)),
                 255};
-
-            glowedBuffer.at(x, y) = c;
+            verticalPass.at(x, y) = c;
         }
     }
 
-    return glowedBuffer;
+    return verticalPass;
 }
+
+} // namespace
 
 void some(FontRasterizer& rr, size_t px)
 {
     const std::string s = "./junk/" + std::to_string(px) + "px.png";
     const std::string text{"makemeglow"};
-    writePng(s, glowText(rr.rasterize(text, px)));
+    const std::vector<float> weights{0.227027f, 0.1945946f, 0.1216216f, 0.054054f, 0.016216f};
+    writePng(s, gaussianBlur(rr.rasterize(text, px), weights));
 }
 
 void foo()
@@ -72,6 +82,7 @@ void foo()
     some(rr, 80);
     some(rr, 100);
     some(rr, 150);
+    some(rr, 200);
 }
 
 } // namespace glow
