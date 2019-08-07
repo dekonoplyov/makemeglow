0682e7b5b2c9efdef0329c0ad47837a17b503426
Denis Konoplev <dekonoplyov@gmail.com>
2019-08-07 15:44:57 +0300

    Font rasterizer tests

---
+++ tests/data/font/README.md
@@ -0,0 +1,2 @@
+If you find non open source font in this project, please, contact me as soon as possible.
+I have no intention to stole someone's work. Just testing font rendering.
--- tests/font_rasterizer_test.cpp
+++ tests/font_rasterizer_test.cpp
@@ -1,13 +1,57 @@
 #include "font_rasterizer.h"
+#include "test_data.h"
 
 #include <gtest/gtest.h>
+
+std::string fontPath(const std::string& fontname, bool scalable = true)
+{
+    return std::string{TEST_DATA_PATH} + "/font/"
+        + (scalable ? "scalable" : "non_scalable")
+        + "/" + fontname;
+}
 
 TEST(FontRasterizerTest, NonExistentFont)
 {
     ASSERT_THROW(glow::FontRasterizer{"/non/existent/font.ttf"}, std::runtime_error);
 }
 
-// TODO
-// throw non scalable font
-// non ascii letters
-//  
+TEST(FontRasterizerTest, CorruptedFont)
+{
+    ASSERT_THROW(glow::FontRasterizer{fontPath("corrupted-cherry-10-r.bdf", false)},
+        std::runtime_error);
+}
+
+TEST(FontRasterizerTest, ExistentFont)
+{
+    ASSERT_NO_THROW(glow::FontRasterizer{fontPath("Farro-Light.ttf")});
+    ASSERT_NO_THROW(glow::FontRasterizer{fontPath("Hado2-Regular.otf")});
+    ASSERT_NO_THROW(glow::FontRasterizer{fontPath("NotoSansJP-Black.otf")});
+    ASSERT_NO_THROW(glow::FontRasterizer{fontPath("cherry-10-r.bdf", false)});
+}
+
+TEST(FontRasterizerTest, ScaleNonScalableFont)
+{
+    auto rr = glow::FontRasterizer{fontPath("cherry-10-r.bdf", false)};
+
+    ASSERT_THROW(rr.rasterize("", 11), std::runtime_error);
+    ASSERT_NO_THROW(rr.rasterize("", 10));
+}
+
+TEST(FontRasterizerTest, CheckMargins)
+{
+    auto rr = glow::FontRasterizer{fontPath("Farro-Light.ttf")};
+
+    for (auto margin : {0, 10}) {
+        auto intensities = rr.rasterize("", /*pxSize*/ 42, margin);
+        ASSERT_EQ(intensities.width(), 2 * margin);
+        ASSERT_EQ(intensities.height(), 2 * margin);
+    }
+}
+
+TEST(FontRasterizerTest, NonASCII)
+{
+    auto rr = glow::FontRasterizer{fontPath("Farro-Light.ttf")};
+    ASSERT_NO_THROW(rr.rasterize("привет", 10));
+}
+
+// TODO one color png write
