151ca92a75cda07b3be7168e4b2715dffc44b738
Denis Konoplev <dekonoplyov@gmail.com>
2019-08-09 17:51:25 +0300

    Test png comprasion

--- tests/test_data.h
+++ tests/test_data.h
@@ -40,7 +40,17 @@ bool isEquals(const Buffer<T>& l, const Buffer<T>& r)
         return false;
     }
 
-    return std::equal(l.data().cbegin(), l.data().cend(), r.data().cbegin());
+    // TODO consider other ways to compare png with threshold
+    size_t mismatches = 0;
+    for (size_t y = 0; y < l.height(); ++y) {
+        for (size_t x = 0; x < l.width(); ++x) {
+            if (l.at(x, y) != r.at(x, y)) {
+                mismatches++;
+            }
+        }
+    }
+
+    return mismatches * 20 < l.data().size();
 }
 
 } // namespace glow
